bff5fecaa62e39d54be4df767daed4292e7cf6c6
MakeCat <hansexpress@outlook.com>
2025-08-02 02:27:27 +0800

    1.0.3

--- src/client/java/make/nohitdelay/MixinClientPlayerInteractionManager.java
+++ src/client/java/make/nohitdelay/MixinClientPlayerInteractionManager.java
@@ -2,14 +2,16 @@ package make.nohitdelay;
 
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class MixinClientPlayerInteractionManager {
-    @Inject(method = "hasLimitedAttackSpeed", at = @At("HEAD"), cancellable = true)
-    void hookHasLimitedAttackSpeed(CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(false);
+    /**
+     * @author MakeCat
+     * @reason simple
+     */
+    @Overwrite
+    public boolean hasLimitedAttackSpeed() {
+        return false;
     }
 }
